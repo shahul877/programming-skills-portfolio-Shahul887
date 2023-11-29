5ecad93f49c6c5f3bd4e0214f9101ecdda092ce2
shahul877 <145225439+shahul877@users.noreply.github.com>
2023-11-29 11:37:49 +0400

    Create Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,10 @@
+guests = ['ifran', 'misbah', 'jusail'] 
+
+name = guests[0].title() 
+print(name + ", would u join for dinner tonight.") 
+
+name = guests[1].title() 
+print (name + ", would u join for dinner tonight.") 
+
+name = guests[2].title() 
+print(name +", would u join for dinner tonight.")
