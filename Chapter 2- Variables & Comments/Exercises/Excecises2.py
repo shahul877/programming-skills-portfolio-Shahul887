8edf28ab2dff375a821b5ca901948231b4ddb653
shahul877 <145225439+shahul877@users.noreply.github.com>
2023-11-29 11:28:00 +0400

    Create Excecises2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Excecises2.py
@@ -0,0 +1,5 @@
+#Find a quote from a famous person you admire. Print the quote and the name of its author. 
+
+
+quote=("jimi hedsix once said,""'knowledge speaks but wisdom listons'")
+print (quote)
