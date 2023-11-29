267922ef45df369b09a93d274d6a2df207b7337f
shahul877 <145225439+shahul877@users.noreply.github.com>
2023-11-29 11:56:22 +0400

    Create Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, message):
+    print(f"\nI'm going to make a {size} t-shirt.")
+    print(f'It will say, "{message}"')
+
+make_shirt('medium', "Keep calm and code on.")
+make_shirt(message="Life is short, smile while you still have teeth.", size='small')
