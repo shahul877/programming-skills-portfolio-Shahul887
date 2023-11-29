42a48b0a0517e6b8e04d648522920289c6090db8
shahul877 <145225439+shahul877@users.noreply.github.com>
2023-11-29 11:46:41 +0400

    Create Exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,22 @@
+glossary = {
+    "Variable": "A named storage location in the computer's memory, used to store and manipulate data during program execution.",
+    "Function": "A reusable block of code that performs a specific task. Functions help in organizing code and promoting reusability.",
+    "Loop": "A control flow statement that allows code to be repeatedly executed. Common types include 'for' loops and 'while' loops.",
+    "Conditional Statement": "A programming construct that performs different actions based on whether a certain condition evaluates to true or false.",
+    "Algorithm": "A step-by-step set of instructions or rules to solve a specific problem or accomplish a particular task in a finite amount of time."
+    }
+
+word = 'Variable'
+print(f"\n{word}: {glossary[word]}")
+
+word = 'Function'
+print(f"\n{word}: {glossary[word]}")
+
+word = 'Loop'
+print(f"\n{word}: {glossary[word]}")
+
+word = 'Conditional Statement'
+print(f"\n{word}: {glossary[word]}")
+
+word = 'Algorithm'
+print(f"\n{word}: {glossary[word]}")
