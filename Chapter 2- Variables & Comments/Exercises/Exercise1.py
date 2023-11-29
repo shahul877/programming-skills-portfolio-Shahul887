52a3454498035e748dc99f97984ce55fd2b41559
shahul877 <145225439+shahul877@users.noreply.github.com>
2023-11-29 11:26:35 +0400

    Create Exercise1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise1.py
@@ -0,0 +1,8 @@
+#Assign a message to a variable, and print that message.
+#Then change the value of the variable to a new message, and print the new
+#imessage.
+
+statement="the real world"
+print(statement)
+statement="the real world matrix"
+print(statement)
