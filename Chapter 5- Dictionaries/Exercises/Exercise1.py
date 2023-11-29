51cef9f7e21cd33f92b42f13008ed9a230fa1622
shahul877 <145225439+shahul877@users.noreply.github.com>
2023-11-29 11:43:52 +0400

    Create Exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,11 @@
+a = {
+    'first_name': 'Abdu',
+    'last_name': 'thufail',
+    'age': 20,
+    'city': 'ajman',
+    }
+
+print(a['first_name'])
+print(a['last_name'])
+print(a['age'])
+print(a['city'])
